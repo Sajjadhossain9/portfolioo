fe5b09e58ae94be96b14ab73e33bf577767a1811
Sajjad Hossain <sajjad00288@gmail.com>
2026-08-25 18:50:11 +0600

    Change header from h3 to h2 in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -297,10 +297,10 @@ export default function App() {
             <span className="nominal">SYSTEMS NOMINAL</span>
           </div>
 
-          <h3>
+          <h2>
             A FULL STACK WEB DEVELOPER WITH KEEN INTEREST IN
             <span> ELECTRONICS AND EMBEDDED SYSTEMS</span>
-          </h3>
+          </h2>
 
           <p>
             I am an Avionics Engineering student with a keen interest in electronics,
